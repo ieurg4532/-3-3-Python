7d02f0b5a074335c93df0c6b63930a9a0b5fddc3
Matrosov <Matrosov@KOKSFENMETT>
2024-11-01 02:56:54 +0100

    Add project files.

---
+++ №3(3) Python/_3_3__Python.py
@@ -0,0 +1,9 @@
+﻿b = str(input("Введіть речення: "))
+
+words = b.split()
+
+word = max(words, key=len)
+if len(word) > 10:
+    print("Найдовше слово має більше 10 символів")
+else:
+    print("Найдовше слово має 10 символів або менше")
